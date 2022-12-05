bc63ad844a8e7641fa4ef551741740703bab57ed
labulac <740162752@qq.com>
2022-12-05 22:42:04 +0800

    use

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## ikun-go
-### 爱坤键盘 敲击J、N、T、M 试试？
-### 使用-s install参数注册服务(管理员)
+### ikun-kryboard.  Try to click J、N、T、M ?
+### use `-s install` to install as service
--- main.go
+++ main.go
@@ -1,130 +1,76 @@
 package main
 
 import (
-	"flag"
+	"fmt"
 	"github.com/kardianos/service"
 	hook "github.com/robotn/gohook"
 	"ikun-go/player"
 	"log"
-	"os/exec"
+	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
-// 服务管理
-var serviceType = flag.String("s", "", "服务管理, 支持install, uninstall")
+type program struct {
+}
+
+func (p *program) Start(s service.Service) error {
+	go p.run()
+	return nil
+}
+
+func (p *program) run() {
+
+	fmt.Println("fsdfasdfsd")
+
+	//code here
+	jntm()
+	wg.Done()
+}
+
+func (p *program) Stop(s service.Service) error {
+	return nil
+}
+
+var wg sync.WaitGroup
 
 func main() {
-	flag.Parse()
-
-	switch *serviceType {
-	case "install":
-		installService()
-	case "uninstall":
-		uninstallService()
-	default:
-
-		s := getService()
-		status, _ := s.Status()
-		if status != service.StatusUnknown {
-			// 以服务方式运行
-			s.Run()
-		} else {
-			// 非服务方式运行
-			switch s.Platform() {
-			case "windows-service":
-				log.Println("可使用 .\\ikun-go.exe -s install 安装服务运行")
-			default:
-				log.Println("可使用 sudo ./ikun-go -s install 安装服务运行")
-			}
-			jntm()
-		}
-	}
-
-}
-
-type program struct{}
-
-func (q *program) Start(s service.Service) error {
-	// Start should not block. Do the actual work async.
-	go q.run()
-	return nil
-}
-func (q *program) run() {
-	log.Println("服务运行")
-	jntm()
-}
-func (q *program) Stop(s service.Service) error {
-	// Stop should not block. Return with a few seconds.
-	return nil
-}
-
-func getService() service.Service {
-	options := make(service.KeyValue)
-	if service.ChosenSystem().String() == "unix-systemv" {
-		options["SysvScript"] = sysvScript
-	}
-
+	wg.Add(1)
 	svcConfig := &service.Config{
-		Name:        "ikun-go",
-		DisplayName: "ikun-go",
-		Description: "author:labulac@88.com",
-		Option:      options,
-		Arguments:   []string{},
+		Name:        "test-service",
+		DisplayName: "test-service",
+		Description: "test-service",
 	}
-
 	prg := &program{}
 	s, err := service.New(prg, svcConfig)
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println(err.Error())
 	}
-	return s
-}
-
-// 卸载服务
-func uninstallService() {
-	s := getService()
-	s.Stop()
-	if service.ChosenSystem().String() == "unix-systemv" {
-		if _, err := exec.Command("/etc/init.d/ikun-go", "stop").Output(); err != nil {
-			log.Println(err)
-		}
-	}
-	if err := s.Uninstall(); err == nil {
-		log.Println("ikun-go 服务卸载成功!")
-	} else {
-		log.Printf("ikun-go 服务卸载失败, ERR: %s\n", err)
-	}
-}
-
-// 安装服务
-func installService() {
-	s := getService()
-
-	status, err := s.Status()
-	if err != nil && status == service.StatusUnknown {
-		// 服务未知，创建服务
-		if err = s.Install(); err == nil {
-			s.Start()
-
-			log.Println("安装 ikun-go 服务成功!")
-			if service.ChosenSystem().String() == "unix-systemv" {
-				if _, err := exec.Command("/etc/init.d/ikun-go", "enable").Output(); err != nil {
-					log.Println(err)
-				}
-				if _, err := exec.Command("/etc/init.d/ikun-go", "start").Output(); err != nil {
-					log.Println(err)
-				}
+	if len(os.Args) > 1 {
+		if os.Args[1] == "install" {
+			x := s.Install()
+			if x != nil {
+				fmt.Println("error:", x.Error())
+				return
 			}
+			fmt.Println("service installed")
+			return
+		} else if os.Args[1] == "uninstall" {
+			x := s.Uninstall()
+			if x != nil {
+				fmt.Println("error:", x.Error())
+				return
+			}
+			fmt.Println("service uninstall success")
 			return
 		}
-
-		log.Printf("安装 ikun-go 服务失败, ERR: %s\n", err)
 	}
-
-	if status != service.StatusUnknown {
-		log.Println("ikun-go 服务已安装, 无需再次安装")
+	err = s.Run()
+	if err != nil {
+		fmt.Println(err.Error())
 	}
+	wg.Wait()
 }
 
 var lastNum int
@@ -170,67 +116,3 @@ func jntm() {
 	s := hook.Start()
 	<-hook.Process(s)
 }
-
-const sysvScript = `#!/bin/sh /etc/rc.common
-DESCRIPTION="{{.Description}}"
-cmd="{{.Path}}{{range .Arguments}} {{.|cmd}}{{end}}"
-name="ikun-go"
-pid_file="/var/run/$name.pid"
-stdout_log="/var/log/$name.log"
-stderr_log="/var/log/$name.err"
-START=99
-get_pid() {
-    cat "$pid_file"
-}
-is_running() {
-    [ -f "$pid_file" ] && cat /proc/$(get_pid)/stat > /dev/null 2>&1
-}
-start() {
-	if is_running; then
-		echo "Already started"
-	else
-		echo "Starting $name"
-		{{if .WorkingDirectory}}cd '{{.WorkingDirectory}}'{{end}}
-		$cmd >> "$stdout_log" 2>> "$stderr_log" &
-		echo $! > "$pid_file"
-		if ! is_running; then
-			echo "Unable to start, see $stdout_log and $stderr_log"
-			exit 1
-		fi
-	fi
-}
-stop() {
-	if is_running; then
-		echo -n "Stopping $name.."
-		kill $(get_pid)
-		for i in $(seq 1 10)
-		do
-			if ! is_running; then
-				break
-			fi
-			echo -n "."
-			sleep 1
-		done
-		echo
-		if is_running; then
-			echo "Not stopped; may still be shutting down or shutdown may have failed"
-			exit 1
-		else
-			echo "Stopped"
-			if [ -f "$pid_file" ]; then
-				rm "$pid_file"
-			fi
-		fi
-	else
-		echo "Not running"
-	fi
-}
-restart() {
-	stop
-	if is_running; then
-		echo "Unable to stop, will not attempt to start"
-		exit 1
-	fi
-	start
-}
-`
