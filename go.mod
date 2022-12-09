8393507edef38ada8a0caefbfb14bac01074beaf
labulac <740162752@qq.com>
2022-12-09 16:29:51 +0800

    add 开机启动

--- go.mod
+++ go.mod
@@ -4,17 +4,34 @@ go 1.19
 
 require (
 	github.com/faiface/beep v1.1.0
-	github.com/kardianos/service v1.2.2
+	github.com/gin-gonic/gin v1.8.1
 	github.com/robotn/gohook v0.40.0
 )
 
 require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.0 // indirect
 	github.com/hajimehoshi/oto v1.0.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
 	github.com/vcaesar/keycode v0.10.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/exp v0.0.0-20190306152737-a1d7652674e8 // indirect
 	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067 // indirect
 	golang.org/x/mobile v0.0.0-20190415191353-3e0bab5405d6 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,76 +1,132 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
-	"github.com/kardianos/service"
+	"github.com/gin-gonic/gin"
 	hook "github.com/robotn/gohook"
 	"ikun-go/player"
 	"log"
 	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"runtime"
+	"strconv"
 	"strings"
-	"sync"
+	"syscall"
 	"time"
 )
 
-type program struct {
-}
+// 服务管理
+var serviceType = flag.String("s", "", "服务管理, 支持install, uninstall")
 
-func (p *program) Start(s service.Service) error {
-	go p.run()
-	return nil
-}
-
-func (p *program) run() {
-
-	fmt.Println("fsdfasdfsd")
-
-	//code here
-	jntm()
-	wg.Done()
-}
-
-func (p *program) Stop(s service.Service) error {
-	return nil
-}
-
-var wg sync.WaitGroup
+var port = "9882"
 
 func main() {
-	wg.Add(1)
-	svcConfig := &service.Config{
-		Name:        "test-service",
-		DisplayName: "test-service",
-		Description: "test-service",
-	}
-	prg := &program{}
-	s, err := service.New(prg, svcConfig)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	if len(os.Args) > 1 {
-		if os.Args[1] == "install" {
-			x := s.Install()
-			if x != nil {
-				fmt.Println("error:", x.Error())
-				return
-			}
-			fmt.Println("service installed")
-			return
-		} else if os.Args[1] == "uninstall" {
-			x := s.Uninstall()
-			if x != nil {
-				fmt.Println("error:", x.Error())
-				return
-			}
-			fmt.Println("service uninstall success")
-			return
+	flag.Parse()
+
+	switch *serviceType {
+	case "install":
+		err := installService(true)
+		if err != nil {
+			log.Println(err)
 		}
+
+		return
+	case "uninstall":
+		err := installService(false)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	default:
+		log.Println("使用 -s install参数增加开机自启动")
+		gin.SetMode(gin.ReleaseMode)
+		r := gin.Default()
+		go func() {
+			err := r.Run(":" + port)
+			if err != nil {
+				panic(err)
+			}
+		}()
+		jntm()
+
 	}
-	err = s.Run()
+
+}
+
+const (
+	winBat = `start %s`
+)
+
+func installService(on bool) error {
+	var err error
+	var path, content string
+	current, err := user.Current()
 	if err != nil {
-		fmt.Println(err.Error())
+		return err
 	}
-	wg.Wait()
+	switch runtime.GOOS {
+
+	case "windows":
+		path = fmt.Sprintf("%s\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\ikun-go.bat", current.HomeDir)
+		abs, _ := filepath.Abs(os.Args[0])
+		//log.Println(abs)
+		content = fmt.Sprintf(winBat, abs)
+		if on {
+			exec.Command(abs).Start()
+		}
+
+	default:
+		return errors.New("不支持的系统")
+	}
+	return writer(on, path, content)
+
+}
+
+func writer(on bool, path, content string) error {
+	var err error
+	if on {
+		stat, _ := os.Stat(path)
+		if stat == nil {
+			err = os.WriteFile(path, []byte(content), os.ModePerm)
+		}
+	} else {
+		err = os.Remove(path)
+		switch runtime.GOOS {
+
+		case "windows":
+			cmd := exec.Command("tasklist")
+			output, err := cmd.Output()
+			//log.Println(string(output))
+			if err != nil {
+				fmt.Println(err)
+				return err
+			}
+
+			// 查找进程ID
+			pid := findPID(output)
+			if pid == -1 {
+				fmt.Println("找不到使用该端口的进程")
+				return nil
+			}
+
+			// 杀死进程
+			cmd = exec.Command("taskkill", "/F", "/PID", fmt.Sprintf("%d", pid))
+			err = cmd.Run()
+			if err != nil {
+				fmt.Println(err)
+				return nil
+			}
+		default:
+			command := fmt.Sprintf("lsof -i tcp:%s | grep LISTEN | awk '{print $2}' | xargs kill -9", port)
+			execCmd(exec.Command("bash", "-c", command))
+		}
+
+	}
+	return err
 }
 
 var lastNum int
@@ -93,6 +149,7 @@ func paintedEggShell(num int) {
 }
 
 func jntm() {
+	log.Println("开始")
 	// 监听键盘事件
 	hook.Register(hook.KeyDown, []string{}, func(e hook.Event) {
 		key := strings.ToUpper(string(e.Keychar))
@@ -115,4 +172,46 @@ func jntm() {
 	})
 	s := hook.Start()
 	<-hook.Process(s)
+}
+
+// Execute command and return exited code.
+func execCmd(cmd *exec.Cmd) {
+	var waitStatus syscall.WaitStatus
+	if err := cmd.Run(); err != nil {
+		if err != nil {
+			os.Stderr.WriteString(fmt.Sprintf("Error: %s\n", err.Error()))
+		}
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus = exitError.Sys().(syscall.WaitStatus)
+			fmt.Printf("Error during killing (exit code: %s)\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+		}
+	} else {
+		waitStatus = cmd.ProcessState.Sys().(syscall.WaitStatus)
+		fmt.Printf("Port successfully killed (exit code: %s)\n", []byte(fmt.Sprintf("%d", waitStatus.ExitStatus())))
+	}
+}
+
+func findPID(output []byte) int {
+	// 在输出中查找进程ID
+	// 这里假设输出是以空格分隔的列表，其中第一列是进程名称，第二列是进程ID
+	// 例如：
+	// process1  1234
+	// process2  5678
+	lines := strings.Split(string(output), "\n")
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		if len(fields) < 2 {
+			continue
+		}
+
+		if fields[0] == "ikun-go.exe" {
+			pid, err := strconv.Atoi(fields[1])
+			if err != nil {
+				continue
+			}
+			return pid
+		}
+	}
+
+	return -1
 }
